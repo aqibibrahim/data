c435fcca8b47479d9f23c11c1f5b9cee6d2e9f36
silicon <silicon@silicon-PC>
2015-01-02 18:06:10 +0500

    Merge origin/master
    Conflicts:
    	README.md

--- parse.js
+++ parse.js
@@ -4,36 +4,10 @@
  * and open the template in the editor.
  */
 
-var base_url = 'http://portanaturasi.com/wp-admin/admin-ajax.php?action=getCustomerList&somethingimportant=whatissoimportant&format=json';
- 
-function example()
-{
-var response = "";
-var form_data = {
-username: username,
-password: password
-};
-$.ajax({
-type: "POST",
-url: base_url,
-data: form_data,
-success: function(response)
-{
-/*response = '[{"Language":"jQuery","ID":"1"},{"Language":"C#","ID":"2"},
-{"Language":"PHP","ID":"3"},{"Language":"Java","ID":"4"},
-{"Language":"Python","ID":"5"},{"Language":"Perl","ID":"6"},
-{"Language":"C++","ID":"7"},{"Language":"ASP","ID":"8"},
-{"Language":"Ruby","ID":"9"}]'*/
-console.log(response);
-var json_obj = $.parseJSON(response);//parse JSON
-var output="<ul>";
-for (var i in json_obj)
-{
-output+="<li>" + json_obj[i].Language + ", " + json_obj[i].ID + "</li>";
+var myMessage = '{"success": true,"counters": [{"counter_name": "dsd", "counter_type": "sds", "counter_unit": "sds" }, { "counter_name": "gdg", "counter_type": "dfd", "counter_unit": "ds" }, { "counter_name": "sdsData", "counter_type": "sds", "counter_unit": " dd " }, { "counter_name": "Stoc final", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "Consum GPL", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "sdg", "counter_type": "dfg", "counter_unit": "gfgd" }, { "counter_name": "dfgd", "counter_type": "fgf", "counter_unit": "liggtggggri " }, { "counter_name": "fgd", "counter_type": "dfg", "counter_unit": "kwfgf " }, { "counter_name": "dfg", "counter_type": "dfg", "counter_unit": "dg" }, { "counter_name": "gd", "counter_type": "dfg", "counter_unit": "dfg" } ] }';
+
+var jsonData = JSON.parse(myMessage);
+for (var i = 0; i < jsonData.counters.length; i++) {
+    var counter = jsonData.counters[i];
+    console.log(counter.counter_name);
 }
-output+="</ul>";
-$('span').html(output);
-},
-dataType: "json"//set to JSON
-})
-} 
