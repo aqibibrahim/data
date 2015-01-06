535704e772009f242c27dd23a53503c5b621855d
silicon <silicon@silicon-PC>
2015-01-06 10:03:59 +0500

    Merge origin/master
    Conflicts:
    	README.md

--- parse.js
+++ parse.js
@@ -1,13 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-var myMessage = '{"success": true,"counters": [{"counter_name": "dsd", "counter_type": "sds", "counter_unit": "sds" }, { "counter_name": "gdg", "counter_type": "dfd", "counter_unit": "ds" }, { "counter_name": "sdsData", "counter_type": "sds", "counter_unit": " dd " }, { "counter_name": "Stoc final", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "Consum GPL", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "sdg", "counter_type": "dfg", "counter_unit": "gfgd" }, { "counter_name": "dfgd", "counter_type": "fgf", "counter_unit": "liggtggggri " }, { "counter_name": "fgd", "counter_type": "dfg", "counter_unit": "kwfgf " }, { "counter_name": "dfg", "counter_type": "dfg", "counter_unit": "dg" }, { "counter_name": "gd", "counter_type": "dfg", "counter_unit": "dfg" } ] }';
-
-var jsonData = JSON.parse(myMessage);
-for (var i = 0; i < jsonData.counters.length; i++) {
-    var counter = jsonData.counters[i];
-    console.log(counter.counter_name);
+function appReady() {
+    alert("verified");
+    var API = "http://portanaturasi.com/wp-admin/admin-ajax.php?action=export_customer_sorting&format=json&delivery_date=13/11/2014";
+    check(API);
 }
+
+function check(API) {
+    alert("entered");
+    var http_request = new XMLHttpRequest();
+    alert(http_request);
+    http_request.open("GET", API, false);
+    http_request.send(null);
+    var my_JSON_object = http_request.responseText;
+    alert(my_JSON_object);
+    var data = JSON.parse(my_JSON_object);
+    var itemList = json.Status.itemlist;
+for(var i=0;i<itemList.length;i++)
+{
+    alert('Image:'+itemList[i].image
+     +'\nID:'+itemList[i].id
+        +'\nName:'+itemList[i].Name);
+}
+    alert("id");
+}
+document.addEventListener("deviceready", appReady, false);
